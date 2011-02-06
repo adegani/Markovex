dab9de8fc7be713c893d7652e33b4d2675bd667a
Alessio Degani <alessio.degani@gmail.com>
2011-02-06 17:20:53 +0100

    Now parse midi files and generate 2nd order markov chain :)

--- include/markovex/markov_synthesis.py
+++ include/markovex/markov_synthesis.py
@@ -17,33 +17,108 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-from numpy import *
-import string
+import sys
+import alsaseq
+import alsamidi
+import time
+from utils import *
 
-#Some costants for friednly note printing
-OCTAVE_MAX_VALUE = 12
-NOTE_NAMES = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
-NOTE_PER_OCTAVE = len( NOTE_NAMES )
+global VERBOSE
 
-def noteNameFromNum(value):
-	noteidx = value % NOTE_PER_OCTAVE
-	octidx = value / OCTAVE_MAX_VALUE
-	name = NOTE_NAMES[int(noteidx)]
-	return name
 
-def printTable(table):
-	'Print a Markov Chain Probabilty Matrix in a readable way'
-	(numRows,numCols)=shape(table)
-	hr=''.join(repeat("--------",numCols))+"-"
-	print hr
-	head = ["| state\t| "]
-	for i in range(numRows):
-		head.append(noteNameFromNum(table[i,0])+"\t| ")
-	print ''.join(head)
-	print hr
-	for r in range(numRows):
-		buff=["| ", noteNameFromNum(table[r,0]), "\t| "]
-		for c in range(1,numCols):
-			buff.append(str(table[r,c]) + "\t| ")
-		print ''.join(buff)
-	print hr
+def playTable(table,first_state,bpm,alsaseq,CH):
+	#init markov chain
+	start_state=first_state
+	curr_state=start_state
+	print bpm
+	input()
+	#populating the list of available notes
+	noteList=table[first_state].keys()
+	
+	#Whait time calculated from BPM
+	tempo=float(1/(float(bpm)/60))
+	input()
+	if (VERBOSE):
+		print "tempo: "+str(tempo)+"\""
+		print "notelist"+str(noteList)
+	
+	#Firts 2 note are the first_state note
+	ev=alsamidi.noteonevent(CH, int(first_state[0]), 127)
+	alsaseq.output(ev)
+	time.sleep(tempo)
+	ev=alsamidi.noteoffevent(CH, int(first_state[0]), 127)
+	alsaseq.output(ev)
+	time.sleep(tempo)
+	while 1:
+		#pick random probability (~uniform[0,1])
+		rand_p=float(random.rand(1)[0])
+
+		pdf=table[curr_state].values()
+		
+		pdf_cumulative=0
+		count=0
+		for p in pdf:
+			pdf_cumulative+=p
+			if (rand_p<=pdf_cumulative):
+				break
+			count+=1
+		
+		if (count>len(noteList)-1):
+			#Trap state!! Fall back to start state
+			next_state=first_state
+			
+			if (VERBOSE):
+				print bcolors.FAIL+"Trap!!!"+bcolors.ENDC
+			
+			#recalculate next note
+			pdf=table[first_state].values()
+			pdf_cumulative=0
+			count=0
+			for p in pdf:
+				pdf_cumulative+=p
+				if (rand_p<=pdf_cumulative):
+					break
+				count+=1
+		else:
+			next_state=(curr_state[1],noteList[count])
+		
+		#evolve...
+		curr_state=next_state
+
+		if (VERBOSE):
+			print "rand:", rand_p
+			print "pdf: "+str(pdf)
+			print "count: "+str(count)
+			print "note: "+str(noteList[count])
+			print "next state:", next_state
+
+		ev=alsamidi.noteonevent(CH, int(noteList[count]), 127)
+		alsaseq.output(ev)
+		time.sleep(tempo)
+
+def readTable(filename):
+	#Read transitions probability matrix from file
+	first=True
+	count=0
+	try:
+		for line in filename:
+			#skip comments, and newline
+			if not (line[0] == "#" or line == "" or line=="\n"):
+				if (first):
+					#First line -> the Header contains some general infos
+					(songName, trackName, bpm, first_state, rows, cols)=eval(line)
+					table={}
+					first=False
+				else:
+					#Read the rest of the table
+					buff=eval(line)
+					table[(buff[0])]=buff[1]
+					count+=1
+		filename.close()
+		print "File successfully read."
+	except Exception:
+		print "Error reading file "+filename.name
+		sys.exit(1)
+	if (VERBOSE):
+		printTable(table,first_state)
+	return (table,first_state,bpm)
---
+++ include/markovex/utils.py
@@ -0,0 +1,78 @@
+#    markovex - Markov chain generative music
+#
+#    General utilities and classes
+#
+#   Copyright (c) 2010 Alessio Degani <alessio.degani@gmail.com>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+from numpy import *
+
+#print color on console output
+class bcolors:
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+
+#Some costants for friednly note printing
+OCTAVE_MAX_VALUE = 12
+NOTE_NAMES = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
+NOTE_PER_OCTAVE = len( NOTE_NAMES )
+
+def noteNameFromNum(value):
+	noteidx = value % NOTE_PER_OCTAVE
+	octidx = value / OCTAVE_MAX_VALUE
+	name = NOTE_NAMES[int(noteidx)]+"-"+str(octidx)
+	return name
+
+def printTable(table,first_state):
+	#Print a Markov Chain Probabilty Matrix in a readable way
+
+	print "Transition probabilty Matrix"
+	numRows=len(table)
+	numCols=len(table[table.keys()[1]])
+	
+	hr='----------------'+''.join(repeat("--------",numCols))+"-"
+	print hr
+	
+	#Table header
+	head = ["| Current\Next\t| "]
+	for item in table[table.keys()[1]]:
+		head.append(noteNameFromNum(item)+"\t| ")
+	print ''.join(head)
+	print hr
+	
+	#print states an transition prob.
+	for state in table:
+		#Green color to identify the first state
+		if (state==first_state):
+			buff=["| "+bcolors.OKGREEN, noteNameFromNum(state[0])+"|"+noteNameFromNum(state[1]), bcolors.ENDC+"\t| "]
+		else:
+			buff=["| ", noteNameFromNum(state[0])+"|"+noteNameFromNum(state[1]), "\t| "]
+		
+		notTrap=0
+		for item in table[state]:
+			buff.append(str(table[state][item]) + "\t| ")
+			notTrap+=table[state][item]
+		
+		#check if trap state (Prob(escape)=0)
+		if (notTrap):
+			line=''.join(buff)
+		else:
+			line=bcolors.FAIL+''.join(buff)+bcolors.ENDC
+		print line
+	print hr
