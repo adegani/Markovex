c69323bf804446d84c5c94f0250d2d50f1de8ab9
Alessio Degani <alessio.degani@gmail.com>
2011-02-09 02:09:42 +0100

    modified out file behavior: if outfile provided, save the chain. Otherwise play the chain

--- include/markovex/markov_synthesis.py
+++ include/markovex/markov_synthesis.py
@@ -25,81 +25,94 @@ from utils import *
 
 global VERBOSE
 
+def playNote(note,duration,alsaseq,CH):
+	#Play a note for a given duration
+	ev=alsamidi.noteonevent(CH, int(note), 127)
+	alsaseq.output(ev)
+	time.sleep(duration)
+	ev=alsamidi.noteoffevent(CH, int(note), 0)
+	alsaseq.output(ev)
 
-def playTable(table,first_state,bpm,alsaseq,CH):
-	#init markov chain
-	start_state=first_state
-	curr_state=start_state
-
+def computeNextState(table,curr_state,first_state):
+	#evolve chain computing next state for given current_state
+	
+	pdf=[]
+	rand_p=0
+	
 	#populating the list of available notes
-	noteList=table[first_state].keys()
+	stateList=table[first_state].keys()
 	
-	#Whait time calculated from BPM
-	tempo=float(1/(float(bpm)/60))
+	if (curr_state==(0,0)):
+		#first step of chain... play first note deterministicaly
+		next_state=(0,first_state[0])
+	elif (curr_state[0]==0 and not(curr_state[1]==0)):
+		#second step of chain... play second note deterministicaly
+		next_state=first_state
+	else:
+		#calculate next state
 	
-	if (VERBOSE):
-		print "tempo: "+str(tempo)+"\""
-		print "notelist"+str(noteList)
-	
-	#Firts 2 note are the first_state note
-	ev=alsamidi.noteonevent(CH, int(first_state[0]), 127)
-	alsaseq.output(ev)
-	time.sleep(tempo)
-	ev=alsamidi.noteoffevent(CH, int(first_state[0]), 0)
-	alsaseq.output(ev)
-	ev=alsamidi.noteonevent(CH, int(first_state[1]), 127)
-	alsaseq.output(ev)
-	time.sleep(tempo)
-	ev=alsamidi.noteoffevent(CH, int(first_state[1]), 0)
-	alsaseq.output(ev)
-	while 1:
 		#pick random probability (~uniform[0,1])
 		rand_p=float(random.rand(1)[0])
 
 		pdf=table[curr_state].values()
-		
+	
 		pdf_cumulative=0
-		count=0
+		next=0
 		for p in pdf:
 			pdf_cumulative+=p
 			if (rand_p<=pdf_cumulative):
 				break
-			count+=1
-		
-		if (count>len(noteList)-1):
+			next+=1
+	
+		if (next>len(stateList)-1):
 			#Trap state!! Fall back to start state
 			next_state=first_state
-			
+		
 			if (VERBOSE):
 				print bcolors.FAIL+"Trap!!!"+bcolors.ENDC
-			
+		
 			#recalculate next note
 			pdf=table[first_state].values()
 			pdf_cumulative=0
-			count=0
+			next=0
 			for p in pdf:
 				pdf_cumulative+=p
 				if (rand_p<=pdf_cumulative):
 					break
-				count+=1
+				next+=1
 		else:
-			next_state=(curr_state[1],noteList[count])
+			next_state=(curr_state[1],stateList[next])
+	
+	#evolve...
+	return next_state
+
+def playTable(pitchTbl,pitchFirstSt,durationTbl,durationFirstSt,bpm,alsaseq,CH):
+	#init markov chain
+	#Note: state (0,0) isn't the first state of the chain, but it's needed to init the chain
+	pitchCurrState=(0,0)
+	durationCurrState=(0,0)
+	
+	#Whait time calculated from BPM
+	tempo=float(1/(float(bpm)/60))
+	
+	#duration in milliseconds of Thirty-second note(1/32)
+	biscroma=float(1000/(float(bpm)/60))/8
+	
+	if (VERBOSE):
+		print "tempo: "+str(tempo)+"\""
+	
+	while(1):
+		(pitchNextState)=computeNextState(pitchTbl,pitchCurrState,pitchFirstSt)
+		(durationNextState)=computeNextState(durationTbl,durationCurrState,durationFirstSt)
+		duration=float((durationNextState[1]*biscroma)/1000)
+
+		playNote(pitchNextState[1],duration,alsaseq,CH)
 		
-		#evolve...
-		curr_state=next_state
-
+		pitchCurrState=pitchNextState
+		durationCurrState=durationNextState
 		if (VERBOSE):
-			print "rand:", rand_p
-			print "pdf: "+str(pdf)
-			print "count: "+str(count)
-			print "note: "+str(noteList[count])
-			print "next state:", next_state
-
-		ev=alsamidi.noteonevent(CH, int(noteList[count]), 127)
-		alsaseq.output(ev)
-		time.sleep(tempo)
-		ev=alsamidi.noteoffevent(CH, int(noteList[count]), 0)
-		alsaseq.output(ev)
+			print "pitchCurrState: "+str(pitchCurrState)
+			print "durationCurrState: "+str(durationCurrState)
 
 def readTable(filename):
 	#Read transitions probability matrix from file
@@ -111,19 +124,24 @@ def readTable(filename):
 			if not (line[0] == "#" or line == "" or line=="\n"):
 				if (first):
 					#First line -> the Header contains some general infos
-					(songName, trackName, bpm, first_state, rows, cols)=eval(line)
-					table={}
+					(songName, trackName, bpm, pitchFirstState, durationFirstState, rows, cols)=eval(line)
+					pitchTbl={}
+					durationTbl={}
 					first=False
 				else:
 					#Read the rest of the table
-					buff=eval(line)
-					table[(buff[0])]=buff[1]
-					count+=1
+					if (count<=rows):
+						#pitch table
+						buff=eval(line)
+						pitchTbl[(buff[0])]=buff[1]
+						count+=1
+					else:
+						#duration table
+						buff=eval(line)
+						durationTbl[(buff[0])]=buff[1]
 		filename.close()
 		print "File successfully read."
 	except Exception:
 		print "Error reading file "+filename.name
 		sys.exit(1)
-	if (VERBOSE):
-		printTable(table,first_state)
-	return (table,first_state,bpm)
+	return (pitchTbl,pitchFirstState,durationTbl,durationFirstState,bpm)
